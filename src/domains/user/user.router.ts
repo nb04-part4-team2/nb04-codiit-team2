b1a1c974269becd2ec6bd1442ad9b2e16c4b7f94
kimhyeyeon <kho1039@naver.com>
2025-12-05 23:28:32 +0900

    fix: 회원가입 validate 부분 추가

--- src/domains/user/user.router.ts
+++ src/domains/user/user.router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { UserController } from './user.controller.js';
+import { UserController } from '@/domains/user/user.controller.js';
 import { asyncHandler } from '@/common/middlewares/asyncHandler.js';
+import { createUserSchema } from '@/domains/user/user.schema.js';
+import { validate } from '@/common/middlewares/validate.middleware.js';
 
 const router = Router();
 const userController = new UserController();
 
-router.post('/', asyncHandler(userController.createUser));
+router.post('/', validate(createUserSchema, 'body'), asyncHandler(userController.createUser));
 
 export default router;
