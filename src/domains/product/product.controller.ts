84c93f766c7280a44ba3d880a5b925568ae22722
InsipidPie1229 <ghdwnsaos@gamil.com>
2025-12-08 18:28:15 +0900

    fix: 명시적 권한 검사 컨트롤러에 추가(상품 등록자가 SELLER인지 확인)

--- src/domains/product/product.controller.ts
+++ src/domains/product/product.controller.ts
@@ -1,14 +1,21 @@
 import { Request, Response } from 'express';
 import { ProductService } from './product.service.js';
 import { CreateProductDto } from './product.dto.js';
-import { UnauthorizedError } from '@/common/utils/errors.js';
+import { UnauthorizedError, ForbiddenError } from '@/common/utils/errors.js';
+import { UserType } from '@prisma/client';
 
 export class ProductController {
   constructor(private productService: ProductService) {}
 
   create = async (req: Request, res: Response) => {
+    // 로그인 여부 확인
     if (!req.user) {
       throw new UnauthorizedError('로그인이 필요합니다.');
+    }
+
+    // 권한(Role) 명시적 검사 추가
+    if (req.user.type !== UserType.SELLER) {
+      throw new ForbiddenError('상품 등록 권한이 없습니다. (판매자만 가능)');
     }
 
     const userId = req.user.id;
