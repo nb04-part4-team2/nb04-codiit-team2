f89d67310431f9c8e4844ec6300e0d493b00ad11
kimhyeyeon <kho1039@naver.com>
2025-12-09 15:03:26 +0900

    feat: 유저 조회/수정 api 구현

--- src/domains/user/user.schema.ts
+++ src/domains/user/user.schema.ts
@@ -7,4 +7,11 @@ export const createUserSchema = z.object({
   type: z.enum(['BUYER', 'SELLER']),
 });
 
+export const updateUserSchema = z.object({
+  name: z.string().min(1, '이름을 입력해주세요.').optional(),
+  password: z.string().min(8, '비밀번호는 8자 이상이어야 합니다.').optional(),
+  currentPassword: z.string().min(1, '현재 비밀번호를 입력해주세요.'),
+});
+
 export type CreateUserDto = z.infer<typeof createUserSchema>;
+export type UpdateUserDto = z.infer<typeof updateUserSchema>;
