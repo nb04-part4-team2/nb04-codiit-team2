4a5cdab8115646b31e08d0cc01172ea7d7c52842
daseul park <seuli0502@gmail.com>
2025-12-10 15:29:28 +0900

    test: 답변 수정 test 구현

---
+++ __tests__/mocks/inquiry.mock.ts
@@ -0,0 +1,12 @@
+const inquiryId = 'inquiry-id-1';
+const replyId = 'reply-id-1';
+const userId = 'user-id-1';
+
+export const mockFindReply = {
+  id: replyId,
+  content: '답변 내용',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  inquiryId: inquiryId,
+  userId: userId,
+};
