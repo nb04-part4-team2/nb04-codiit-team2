6c48afe2b2fac29168aab9336ee8024f862beef6
InsipidPie1229 <ghdwnsaos@gamil.com>
2025-12-10 13:03:43 +0900

    feat: 상품 목록 조회를 위한 라우터 추가

--- src/domains/product/product.router.ts
+++ src/domains/product/product.router.ts
@@ -3,7 +3,7 @@ import { asyncHandler } from '@/common/middlewares/asyncHandler.js';
 import { validate } from '@/common/middlewares/validate.middleware.js';
 import { authenticate, onlySeller } from '@/common/middlewares/auth.middleware.js'; // onlySeller 추가
 import { productController } from './product.container.js';
-import { createProductSchema } from './product.schema.js';
+import { createProductSchema, productListSchema } from './product.schema.js';
 import { nestedInquiryRouter } from '../inquiry/inquiry.router.js';
 
 const productRouter = Router();
@@ -15,6 +15,14 @@ productRouter.post(
   onlySeller, // 판매자 권한 확인 미들웨어 추가
   validate(createProductSchema, 'body'),
   asyncHandler(productController.create),
+);
+
+// 상품 목록 조회 API
+productRouter.get(
+  '/',
+  authenticate,
+  validate(productListSchema, 'query'),
+  asyncHandler(productController.getProducts), // 이제 타입 에러 없이 연결됩니다.
 );
 
 // 문의 중첩 라우터
