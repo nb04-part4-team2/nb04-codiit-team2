1fe8e68df5e52029975ca8b670c38fa8824a4ee4
Park-DaSeul <seuli0502@gmail.com>
2025-12-15 10:20:34 +0900

    inquiry 리팩토링 (#76)
    * refactor: inquiry.mapper 구현
    
    * test: inquiry리팩토링으로 인한 업데이트
    
    * refactor: 맵퍼 서비스-> 컨트롤러로 이동, 테스트 코드 수정
    
    * refactor: inquiry.dto 오타 수정
    
    * refactor: inquiry.mapper 오타 수정

--- src/domains/inquiry/inquiry.controller.ts
+++ src/domains/inquiry/inquiry.controller.ts
@@ -1,6 +1,16 @@
 import type { Request, Response } from 'express';
 import type { InquiryService } from './inquiry.service.js';
 import { UnauthorizedError } from '@/common/utils/errors.js';
+import {
+  toGetInquiriesResponse,
+  toCreateInquiryResponse,
+  toGetAllInquiriesResponse,
+  toGetInquiryByIdResponse,
+  toUpdateInquiryResponse,
+  toDeleteInquiryResponse,
+  toCreateReplyResponse,
+  toUpdateReplyResponse,
+} from './inquiry.mapper.js';
 
 export class InquiryController {
   constructor(private inquiryService: InquiryService) {}
@@ -10,7 +20,7 @@ export class InquiryController {
     const { productId } = req.params;
 
     const inquiries = await this.inquiryService.getInquiries(productId);
-    return res.status(200).json(inquiries);
+    return res.status(200).json(toGetInquiriesResponse(inquiries));
   };
 
   // 문의 생성
@@ -22,7 +32,7 @@ export class InquiryController {
 
     const data = req.body;
     const inquiry = await this.inquiryService.createInquiry(productId, userId, data);
-    return res.status(201).json(inquiry);
+    return res.status(201).json(toCreateInquiryResponse(inquiry));
   };
 
   // 모든 문의 조회 (사용자 본인의 문의)
@@ -33,7 +43,7 @@ export class InquiryController {
     const userId = req.user.id;
 
     const inquiries = await this.inquiryService.getAllInquiries(query, userId);
-    return res.status(200).json(inquiries);
+    return res.status(200).json(toGetAllInquiriesResponse(inquiries));
   };
 
   // 특정 문의 조회
@@ -41,7 +51,7 @@ export class InquiryController {
     const { id } = req.params;
 
     const inquiry = await this.inquiryService.getInquiryById(id);
-    return res.status(200).json(inquiry);
+    return res.status(200).json(toGetInquiryByIdResponse(inquiry));
   };
 
   // 문의 수정
@@ -53,7 +63,7 @@ export class InquiryController {
 
     const data = req.body;
     const inquiry = await this.inquiryService.updateInquiry(id, userId, data);
-    return res.status(200).json(inquiry);
+    return res.status(200).json(toUpdateInquiryResponse(inquiry));
   };
 
   // 문의 삭제
@@ -64,7 +74,7 @@ export class InquiryController {
     const userId = req.user.id;
 
     const inquiry = await this.inquiryService.deleteInquiry(id, userId);
-    return res.status(200).json(inquiry);
+    return res.status(200).json(toDeleteInquiryResponse(inquiry));
   };
 
   // 답변 생성
@@ -76,7 +86,7 @@ export class InquiryController {
 
     const data = req.body;
     const reply = await this.inquiryService.createReply(id, userId, data);
-    return res.status(201).json(reply);
+    return res.status(201).json(toCreateReplyResponse(reply));
   };
 
   // 답변 수정
@@ -88,6 +98,6 @@ export class InquiryController {
 
     const data = req.body;
     const reply = await this.inquiryService.updateReply(id, userId, data);
-    return res.status(200).json(reply);
+    return res.status(200).json(toUpdateReplyResponse(reply));
   };
 }
