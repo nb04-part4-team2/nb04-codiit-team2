b05a0f9decf4ebca7518a827c394720559cbe5c9
winnie4869 <lhy6598@naver.com>
2025-12-15 18:52:22 +0900

    feat: s3 pr리뷰 수정

--- src/domains/s3/s3.controller.ts
+++ src/domains/s3/s3.controller.ts
@@ -1,17 +1,17 @@
 import { Request, Response } from 'express';
 import { S3Service } from './s3.service.js';
-import { asyncHandler } from '@/common/middlewares/asyncHandler.js';
+import { BadRequestError } from '@/common/utils/errors.js';
 
 export class S3Controller {
   constructor(private s3Service: S3Service) {}
 
-  uploadImage = asyncHandler(async (req: Request, res: Response) => {
+  uploadImage = async (req: Request, res: Response) => {
     const file = req.file;
     if (!file) {
-      return res.status(400).json({ message: '파일이 제공되지 않았습니다.' });
+      throw new BadRequestError('파일이 제공되지 않았습니다.');
     }
 
     const result = await this.s3Service.uploadImage(file);
     res.status(201).json(result);
-  });
+  };
 }
--- src/domains/s3/s3.service.ts
+++ src/domains/s3/s3.service.ts
@@ -2,10 +2,6 @@ import { uploadFile } from '@/common/utils/s3.util.js';
 
 export class S3Service {
   async uploadImage(file: Express.Multer.File) {
-    if (!file) {
-      throw new Error('업로드할 파일이 없습니다.');
-    }
-
     const { url, key } = await uploadFile({
       buffer: file.buffer,
       originalname: file.originalname,
