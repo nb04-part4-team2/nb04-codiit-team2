93d019d84173a5db43b4add9c879b6c6718cea69
winnie4869 <lhy6598@naver.com>
2025-12-15 10:46:08 +0900

    feat: s3 api 구현

--- src/domains/s3/s3.router.ts
+++ src/domains/s3/s3.router.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { S3Controller } from './s3.controller.js';
 import { S3Service } from './s3.service.js';
 import { upload } from '@/common/middlewares/upload.middleware.js';
-import { mockAuthMiddleware } from '@/common/middlewares/mock.auth.middleware.js';
+import { authenticate } from '@/common/middlewares/auth.middleware.js';
 
 const s3Router = Router();
 const s3Service = new S3Service();
@@ -10,6 +10,6 @@ const s3Controller = new S3Controller(s3Service);
 
 // POST /api/s3/upload
 // TODO: 실제 인증 미들웨어(authMiddleware)로 교체해야 합니다.
-s3Router.post('/upload', mockAuthMiddleware, upload.single('image'), s3Controller.uploadImage);
+s3Router.post('/upload', authenticate, upload.single('image'), s3Controller.uploadImage);
 
 export default s3Router;
