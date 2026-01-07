9f005a0fb32c3c74c2056e50a8cef3f0cf23a9e1
aprkal12 <ehdehd0175@gmail.com>
2026-01-07 12:06:20 +0900

    fix: 장바구니 수정 스키마 변경

--- src/domains/cart/cart.schema.ts
+++ src/domains/cart/cart.schema.ts
@@ -6,23 +6,25 @@ export const cartItemIdParamSchema = z
   })
   .strict();
 
-export const updateCartSchema = z.object({
-  productId: z.cuid({ message: '올바르지 않은 productId 형식입니다.' }),
-  sizes: z
-    .array(
-      z.object({
-        sizeId: z.number().int().min(1).max(6), // 사이즈 스키마 참고
-        quantity: z.number().int().min(1, '수량은 1개 이상이어야 합니다.'),
-      }),
-    )
-    .nonempty('최소 하나의 옵션을 선택해야 합니다.')
-    .refine(
-      (items) => {
-        const sizeIds = items.map((item) => item.sizeId);
-        return new Set(sizeIds).size === sizeIds.length;
-      },
-      { message: '중복된 사이즈 옵션이 존재합니다.' },
-    ),
-});
+export const updateCartSchema = z
+  .object({
+    productId: z.cuid({ message: '올바르지 않은 productId 형식입니다.' }),
+    sizes: z
+      .array(
+        z.object({
+          sizeId: z.number().int().min(1).max(6), // 사이즈 스키마 참고
+          quantity: z.number().int().min(1, '수량은 1개 이상이어야 합니다.'),
+        }),
+      )
+      .nonempty('최소 하나의 옵션을 선택해야 합니다.')
+      .refine(
+        (items) => {
+          const sizeIds = items.map((item) => item.sizeId);
+          return new Set(sizeIds).size === sizeIds.length;
+        },
+        { message: '중복된 사이즈 옵션이 존재합니다.' },
+      ),
+  })
+  .strict();
 
 export type UpdateCartBody = z.infer<typeof updateCartSchema>;
